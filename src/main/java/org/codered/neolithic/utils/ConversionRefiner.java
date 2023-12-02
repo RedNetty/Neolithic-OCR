128531f4c285b1673963449bd3794c08d8c9b94e
Jack <jacktcooN@gmail.com>
2023-12-02 01:39:27 -0600

    First Commit

---
+++ src/main/java/org/codered/neolithic/utils/ConversionRefiner.java
@@ -0,0 +1,65 @@
+package org.codered.neolithic.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ConversionRefiner {
+
+
+    public ConversionRefiner() {
+
+    }
+
+    public String refineConversion(String text) {
+        return correctOcrOutput(text);
+    }
+
+    private String correctOcrOutput(String ocrOutput) {
+        String complexPattern = buildComplexPattern();
+        Pattern pattern = Pattern.compile(complexPattern);
+        Matcher matcher = pattern.matcher(ocrOutput);
+
+        StringBuffer correctedOutput = new StringBuffer();
+        while (matcher.find()) {
+            String match = matcher.group();
+            String correctedMatch = analyzeAndCorrectExpression(match);
+            matcher.appendReplacement(correctedOutput, correctedMatch);
+        }
+        matcher.appendTail(correctedOutput);
+
+        return correctedOutput.toString();
+    }
+
+    private String buildComplexPattern() {
+        // Patterns for various mathematical elements
+        String numberPattern = "([0-9]+\\.?[0-9]*|\\([0-9]+\\.?[0-9]*\\))";
+        String operatorPattern = "([-+*/^:])";
+        String functionPattern = "(sin|cos|tan|log|ln|sqrt)";
+        String complexElementPattern = String.format("(%s|%s)", numberPattern, functionPattern);
+
+        // Complex patterns for combinations of elements
+        String binaryOperationPattern = complexElementPattern + "\\s*" + operatorPattern + "\\s*" + complexElementPattern;
+        String functionWithArgumentPattern = functionPattern + "\\s*\\(" + complexElementPattern + "\\)";
+
+        // Combine all patterns
+        return String.join("|", binaryOperationPattern, functionWithArgumentPattern);
+    }
+
+    private String analyzeAndCorrectExpression(String expression) {
+        // Analyze the structure of the expression and apply corrections based on its components
+        // This is a simplified version of what a parser might do
+
+        // Example: Replace ":" with "/" in division-like patterns
+        String correctedExpression = expression.replaceAll("(?<=\\d)\\s*:\\s*(?=\\d)", "/");
+
+        // Handle other specific cases, such as functions, exponents, and so on
+        // Additional complex logic can be added here
+
+        // Normalize spaces and other formatting issues
+        correctedExpression = correctedExpression.replaceAll("\\s+", "");
+
+        return correctedExpression;
+    }
+
+
+}
---
+++ src/main/java/org/codered/neolithic/utils/ImageEnhancer.java
@@ -0,0 +1,34 @@
+package org.codered.neolithic.utils;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
+import java.awt.image.RescaleOp;
+
+public class ImageEnhancer {
+
+    public static BufferedImage enhanceImage(BufferedImage image) {
+        return sharpenImage(enhanceContrast(image));
+    }
+
+    private static BufferedImage enhanceContrast(BufferedImage originalImage) {
+        RescaleOp rescaleOp = new RescaleOp(1.2f, 15, null); // You can adjust these values
+        BufferedImage contrastEnhancedImage = rescaleOp.filter(originalImage, null);
+        return contrastEnhancedImage;
+    }
+
+    private static BufferedImage sharpenImage(BufferedImage originalImage) {
+        float[] sharpenMatrix = {
+                0.0f, -1.0f, 0.0f,
+                -1.0f, 5.0f, -1.0f,
+                0.0f, -1.0f, 0.0f
+        };
+        BufferedImage sharpenedImage = new BufferedImage(
+                originalImage.getWidth(),
+                originalImage.getHeight(),
+                originalImage.getType());
+        ConvolveOp convolveOp = new ConvolveOp(new Kernel(3, 3, sharpenMatrix));
+        convolveOp.filter(originalImage, sharpenedImage);
+        return sharpenedImage;
+    }
+}
